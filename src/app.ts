5834f32ebd757dc891ea9d5e82fe3e6dd379a08d
chain <klaus.mika1052@gmail.com>
2023-07-31 22:39:06 -0300

    regiter a route to get url file as static

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express'
+import { router } from './routes'
+import { resolve } from 'path'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/data', express.static(resolve(__dirname, '../data')))
+
+app.use(router)
 
 export default app
