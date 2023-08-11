18b2300e2d0f8bf88038b092e6ae62d545ad28f6
chain <klaus.mika1052@gmail.com>
2023-08-11 00:40:05 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express'
 import { router } from './routes'
 import { resolve } from 'path'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  }),
+)
 
 app.use('/data', express.static(resolve(__dirname, '../data')))
 
